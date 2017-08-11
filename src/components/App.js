4e1da9fd047125126e919ea244807befea47ddcb
Mohmed Bamhraz <mohmedbamhrz@gmail.com>
2017-08-11 12:02:19 -0700

    - add submit forms

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
-import QuestionsIndexPage from './pages/QuestionsIndexPage';
 import {
   BrowserRouter as Router, //when importing use `as` to alias imported to something else
   Route,
-  Link
+  Link,
+  Switch
 } from 'react-router-dom'
-import QuestionsShowPage from './pages/QuestionsShowPage'
+import QuestionsIndexPage from './pages/QuestionsIndexPage';
+import QuestionsNewPage from './pages/QuestionsNewPage';
+import QuestionsShowPage from './pages/QuestionsShowPage';
 
 class App extends Component {
   render() {
@@ -18,9 +20,12 @@ class App extends Component {
             <Link to='/questions/new'>New Question</Link>
           </nav>
           <h1>Awesome Answers</h1>
-          <Route exact path='/' component={QuestionsIndexPage} />
-          <Route exact path='/questions' component={QuestionsIndexPage} />
-          <Route path='/questions/:id' component={QuestionsShowPage}/>
+          <Switch>
+            <Route exact path='/' component={QuestionsIndexPage}/>
+            <Route exact path='/questions' component={QuestionsIndexPage}/>
+            <Route path='/questions/new' component={QuestionsNewPage}/>
+            <Route path='/questions/:id' component={QuestionsShowPage}/>
+          </Switch>
         </div>
       </Router>
     );
